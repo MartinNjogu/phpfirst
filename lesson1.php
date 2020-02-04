141c446b2fb789396a546be4592499411eca10c5
MartinNjogu <njogumartin202gmail.com>
2020-02-04 12:06:29 +0300

    first php

---
+++ lesson1.php
@@ -0,0 +1,36 @@
+<?php
+//intro to php
+//php is a widely used, open source scripting language
+//php scrips ae executed on the server
+//php files can contain ex, hml, CSS Javascript and php code
+//php code is executed on the server and the result is returned to the browser as plane HTML
+//php files have extension ".php"
+echo "Hello World";
+echo"<h1 style='color: indianred'>Hello World</h1>";
+
+//echo:print content in browser
+//Hello world : content printed in browser
+//;: end of php statement or instruction
+
+//VARIABLES IN PHP
+//a container for storing data
+$myname="Martin <br>";
+echo $myname;
+$name1="Cisco systems <br>  ";
+$name2="Apple<br> ";
+$name3="Microsoft <br>";
+echo $name1;
+echo $name2;
+echo $name3;
+
+echo"<h1>Hello World</h1>";
+
+
+
+
+
+
+
+?>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores dignissimos eius eveniet inventore iste magni omnis sit, soluta velit. Aliquam delectus, dolor dolorum esse inventore mollitia quas quo suscipit?
+</p>
